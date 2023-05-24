2441f1bcb3b32703cede2e97755f0e274c78059d
Louis Nowell Nicolle <lnowellnicol@spartan-login1.hpc.unimelb.edu.au>
2023-05-24 18:20:50 +1000

    Slurm update

--- LoadedIsolatedFoundingPopulation.sh
+++ LoadedIsolatedFoundingPopulation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 # TODO:change this reference
-source ../../Coding/Bash-Command-Parameter-Ranges-Iterator/BashCommandParameterRangesIterator.sh
+source ../Bash-Command-Parameter-Ranges-Iterator/BashCommandParameterRangesIterator.sh
 
 declare -A paramRanges=(
-  ["migrantsSize"]=$(seq 2 1 30)
-  ["mutsFrequency"]=$(seq 0.01 0.01 0.20)
-  ["mutsCount"]=$(seq 3 1 30)
+  ["migrantsSize"]=$(seq 2 3 30)
+  ["mutsFrequency"]=$(seq 0.01 0.04 0.20)
+  ["mutsCount"]=$(seq 3 3 30)
   ["sterility"]=$(seq 0 1 1)
   ["xlinked"]=$(seq 0 1 1)
 )
@@ -23,4 +23,4 @@ fi
 echo "Result,MutationFrequency,MutationCount,Individuals,Males,Sterile,Xlinked" >> "$outputFile"
 
 
-runOverRanges "slim -d outputFilePath=$outputFile %s LoadedIsolatedFoundingPopulation.slim" "-d" paramRanges
+runOverRanges "slim -d outputFilePath='$outputFile' %s LoadedIsolatedFoundingPopulation.slim" "-d" paramRanges
