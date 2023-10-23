89f040ae4be50208f1531a228a8d6b71c80cae00
4321louis <31239298+4321louis@users.noreply.github.com>
2023-10-23 23:08:11 +1100

    Comparing Different load types! Cool results

--- data_analysis/basicGraphs.r
+++ data_analysis/basicGraphs.r
@@ -7,14 +7,14 @@ library(dplyr)
 #   group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked, survivalRate,count) %>% 
 #   summarise()
 # autosomalData <- groupedData[groupedData$Xlinked==0,]
-groupedGraphData <- read.csv("./data/BHSGraphs60.csv") %>%
-  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked) %>% 
+groupedGraphData <- read.csv("./data/out_GraphSlicesLoadTypeCompare_LIFP_1697426721.csv") %>%
+  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked, FemaleOnlyEffect) %>% 
   mutate(count = n()) %>%
   mutate(survivalRate = sum(Result != "EXTINCT") / count) %>%
-  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked, survivalRate,count) %>% 
+  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked, FemaleOnlyEffect, survivalRate, count) %>% 
   summarise()
 autosomalGraphData <- groupedGraphData[groupedGraphData$Xlinked==0,]
-autosomalGraphData <- filter(autosomalGraphData,MutationFrequency!=0.5)
+# autosomalGraphData <- filter(autosomalGraphData,MutationFrequency!=0.5)
 
 # dataSlice2d.MutationFrequency <- filter(autosomalData, MutationCount==61, Individuals == 22, GrowthRate == 2, Sterile==1)
 # dataSlice2d.MutationCount <- filter(autosomalData, MutationFrequency==0.11, Individuals == 22, GrowthRate == 2, Sterile==1)
@@ -41,15 +41,64 @@ dataSlice2d.MutationFrequency <- filter(autosomalGraphData, MutationCount==100, 
 dataSlice2d.MutationCount <- filter(autosomalGraphData, MutationFrequency==0.05, Individuals == 25, GrowthRate == 3, Sterile==1)
 dataSlice2d.Individuals <- filter(autosomalGraphData, MutationFrequency==0.05, MutationCount==100, GrowthRate == 3, Sterile==1)
 dataSlice2d.GrowthRate <- filter(autosomalGraphData, MutationFrequency==0.05, MutationCount==100, Individuals == 25, Sterile==1)
-
-
+# basic plots
+plot(survivalRate~MutationFrequency,data=autosomalGraphData,ylim=c(0,1))
 
 plot(survivalRate~MutationFrequency,data=dataSlice2d.MutationFrequency,ylim=c(0,1))
 plot(survivalRate~MutationCount,data=dataSlice2d.MutationCount,ylim=c(0,1))
 plot(survivalRate~Individuals,data=dataSlice2d.Individuals,ylim=c(0,1))
 plot(survivalRate~GrowthRate,data=dataSlice2d.GrowthRate,ylim=c(0,1))
 
+# Load comparison
+groupedGraphData <- read.csv("./data/out_GraphSlicesLoadTypeCompare_LIFP_1697426721.csv") %>%
+  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked, FemaleOnlyEffect) %>% 
+  mutate(count = n()) %>%
+  mutate(survivalRate = sum(Result != "EXTINCT") / count) %>%
+  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, Sterile, Xlinked, FemaleOnlyEffect, survivalRate, count) %>% 
+  summarise()
+autosomalGraphData <- groupedGraphData[groupedGraphData$Xlinked==0,]
 
+dataSlice2d.MutationFrequency <- filter(autosomalGraphData, MutationCount==60, Individuals == 20, GrowthRate == 4)
+dataSlice2d.MutationCount <- filter(autosomalGraphData, MutationFrequency==0.10, Individuals == 20, GrowthRate == 4)
+# dataSlice2d.Lethal <- filter(autosomalGraphData, MutationFrequency==0.10, Individuals == 20, GrowthRate == 4)
+dataSlice2d.Individuals <- filter(autosomalGraphData, MutationFrequency==0.10, MutationCount==60, GrowthRate == 4)
+dataSlice2d.GrowthRate <- filter(autosomalGraphData, MutationFrequency==0.10, MutationCount==60, Individuals == 20)
+
+
+plot(survivalRate~MutationFrequency,data=filter(dataSlice2d.MutationFrequency,Sterile==0,FemaleOnlyEffect==1),col="black",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~MutationFrequency,data=filter(dataSlice2d.MutationFrequency,Sterile==0,FemaleOnlyEffect==0),col="blue",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~MutationFrequency,data=filter(dataSlice2d.MutationFrequency,Sterile==1,FemaleOnlyEffect==1),col="red",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~MutationFrequency,data=filter(dataSlice2d.MutationFrequency,Sterile==1,FemaleOnlyEffect==0),col="purple",ylim=c(0,1))
+
+plot(survivalRate~MutationCount,data=filter(dataSlice2d.MutationCount,Sterile==0,FemaleOnlyEffect==1),col="black",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~MutationCount,data=filter(dataSlice2d.MutationCount,Sterile==0,FemaleOnlyEffect==0),col="blue",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~MutationCount,data=filter(dataSlice2d.MutationCount,Sterile==1,FemaleOnlyEffect==1),col="red",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~MutationCount,data=filter(dataSlice2d.MutationCount,Sterile==1,FemaleOnlyEffect==0),col="purple",ylim=c(0,1))
+
+plot(survivalRate~Individuals,data=filter(dataSlice2d.Individuals,Sterile==0,FemaleOnlyEffect==1),col="black",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~Individuals,data=filter(dataSlice2d.Individuals,Sterile==0,FemaleOnlyEffect==0),col="blue",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~Individuals,data=filter(dataSlice2d.Individuals,Sterile==1,FemaleOnlyEffect==1),col="red",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~Individuals,data=filter(dataSlice2d.Individuals,Sterile==1,FemaleOnlyEffect==0),col="purple",ylim=c(0,1))
+
+plot(survivalRate~GrowthRate,data=filter(dataSlice2d.GrowthRate,Sterile==0,FemaleOnlyEffect==1),col="black",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~GrowthRate,data=filter(dataSlice2d.GrowthRate,Sterile==0,FemaleOnlyEffect==0),col="blue",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~GrowthRate,data=filter(dataSlice2d.GrowthRate,Sterile==1,FemaleOnlyEffect==1),col="red",ylim=c(0,1))
+par(new=TRUE)
+plot(survivalRate~GrowthRate,data=filter(dataSlice2d.GrowthRate,Sterile==1,FemaleOnlyEffect==0),col="purple",ylim=c(0,1))
+
+
+# 3d ideas
 library(ggplot2)
 library(scales)
 
