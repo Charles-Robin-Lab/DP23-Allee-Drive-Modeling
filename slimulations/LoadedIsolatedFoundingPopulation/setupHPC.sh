e35895b092fb743f8f15c1b1eebc96db2758f386
4321louis <31239298+4321louis@users.noreply.github.com>
2023-10-31 02:09:50 +1100

    max generations + reorder output file + Lab questions...

--- slimulations/LoadedIsolatedFoundingPopulation/setupHPC.sh
+++ slimulations/LoadedIsolatedFoundingPopulation/setupHPC.sh
@@ -1,5 +1,5 @@
 outputFile=$1
-echo "Seed,Result,Time,MutationFrequency,MutationCount,GrowthRate,RecombinationRate,Individuals,Males,Sterile,Xlinked,FemaleOnlyEffect" >> "$outputFile"
+echo "Seed,Result,Time,Males,Individuals,GrowthRate,RecombinationRate,MaxGenerations,MutationFrequency,MutationCount,Sterile,Xlinked,FemaleOnlyEffect" >> "$outputFile"
 
 
 
