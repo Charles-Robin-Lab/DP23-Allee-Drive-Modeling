c9fcfc1c5a750f3883aeefadc8a614904200136d
4321louis <31239298+4321louis@users.noreply.github.com>
2023-11-02 22:42:38 +1100

    Lab populations sims + Soumitra test case

---
+++ slimulations/LoadedLabPopulation/setupHPC.sh
@@ -0,0 +1,5 @@
+outputFile=$1
+echo "Seed,Result,Time,Males,Individuals,GrowthRate,RecombinationRate,MaxGenerations,MutationFrequency,MutationCount,Sterile,Xlinked,FemaleOnlyEffect" >> "$outputFile"
+
+
+
