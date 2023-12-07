70e6fd5fab1cfd705515e86f613cb28b0b67fd41
4321louis <31239298+4321louis@users.noreply.github.com>
2023-12-07 14:55:22 +1100

    Update Graph generators for paper

--- analytical/numericalSolutions.r
+++ analytical/numericalSolutions.r
@@ -66,15 +66,15 @@ rGeneDrift <- function(Nmuts,prevN,nextN) {
 
 
 
-# a function for generating samples of W, with W_0 = 1
-W <- function(loci, p, Ne, nSamples = 1000) {
+# a function for generating samples of S, with W_0 = 1
+basicSim <- function(loci, p, Ne, nSamples = 1000) {
     phat <- rbinom(loci * nSamples, size = 2 * Ne, prob = p) # exact binomial draw
     phat <- matrix(phat, nrow = loci) # n x nSamples matrix
     #   sample binomial amount of homozygotes
     #   Nhomozygous <- round(Ne*(phat/(2*Ne)^2))
     apply(phat, 2, function(x) {
-        Nhomozygous <- Ne * (x / (2 * Ne))^2
-        prod(1 - Nhomozygous / Ne)
+        Hi <- (x / (2 * Ne))^2
+        prod(1 - Hi)
     })
 }
 
@@ -92,15 +92,19 @@ extendedSim <- function(n, p, Ne, nSamples = 1000) {
     out
 }
 
-extendedSimRecurse <- function(loci, p, Ne, birthRate, carryingCapacity, nSamples = 1000) {
+genotypeSim <- function(n, p, Ne, birthRate, nSamples = 1000) {
     out = rep(Ne, nSamples)
     for (i in 1:nSamples) {
-        #  initial params
-        currentN <- Ne
-        currentMutCounts <- rbinom(loci, size = 2 * Ne, prob = p)
-        # initialise with correct size 
-        currentGenotypeCounts <- matrix(0, 3, loci)
-       while(currentN > 0 && currentN < 80/birthRate) {
+        for (j in 1:loci) {
+            # NMutallele <- rbinom(1, size = 2 * currentN, prob = p)
+            # Nhomozygous <- rHomozygotes(currentN,NMutallele)
+            # currentN <- currentN - Nhomozygous
+            currentMutCounts <- rbinom(loci, size = 2 * Ne, prob = p)
+        }
+        currentN <- round(Ne*birthRate)
+        for (j in 1:loci) {
+            # initialise with correct size 
+            currentGenotypeCounts <- matrix(0, 3, loci)
             # Genotype dists
             for (j in 1:loci) {
                currentGenotypeCounts[1,j] <- rHomozygotes(currentN,currentMutCounts[j])
@@ -114,24 +118,14 @@ extendedSimRecurse <- function(loci, p, Ne, birthRate, carryingCapacity, nSample
                     currentN <- currentN - currentGenotypeCounts[1,j]
                     currentGenotypeCounts[1,j] <- 0
             }
-            # update all loci genotype counts after all the eliminations
-            for(j in 1:loci) {
-                currentGenotypeCounts[,j] <- rmvhyper(1,currentGenotypeCounts[,j],currentN)
-                currentMutCounts[j] <- currentGenotypeCounts[2,j]
-            }
-            # reproduce and drift
-            nextN <- rpois(1,birthRate*currentN)
-            for (j in 1:loci) {
-                currentMutCounts[j] <- rGeneDrift(currentMutCounts[j],currentN,nextN)
-            }
-            currentN <- nextN
        }
-        out[i] <- if (currentN<=0) "EXTINCT" else "SURVIVAL"
+        out[i] <- currentN
     }
     out
 }
 
-extendedSimRecurse2 <- function(loci, p, Ne, birthRate, carryingCapacity, nSamples = 1000) {
+
+recursiveSim <- function(loci, p, Ne, birthRate, carryingCapacity, nSamples = 1000) {
     out = rep(Ne, nSamples)
     for (i in 1:nSamples) {
         #  initial params
@@ -176,22 +170,31 @@ extendedSimRecurse2 <- function(loci, p, Ne, birthRate, carryingCapacity, nSampl
 # some example parameters...
 loci <- 100 # number of loci
 freqRange <- seq(0.0, 0.3, by = 0.0025) # mean frequency in ancestral population
-Ne <- 25
+N0 <- 25
 birthRate <- 3.0
 carryingCapacity <- 1000
 survivalW <- list()
 for (averageMutFreq in freqRange) {
     mutFreqs <- rep(averageMutFreq,loci)
-    fitnessDecreases <- W(loci, mutFreqs, Ne)
-    survivalW <- append(survivalW, length(fitnessDecreases[fitnessDecreases < 2 / birthRate]) / length(fitnessDecreases))
+    lambda <- birthRate*basicSim(loci, mutFreqs, N0)
+    survivalW <- append(survivalW, length(lambda[lambda < 1]) / length(lambda))
 }
 plot(freqRange, survivalW,col="red",ylim=c(0,1),pch=2,xlab="",ylab="")
 par(new=TRUE)
 
-## model 2
+# ## model 2
+# survivalSingle <- list()
+# for (averageMutFreq in freqRange) {
+#     lambda <- (extendedSim(loci, averageMutFreq, birthRate*N0)-N0)/N0
+#     survivalSingle <- append(survivalSingle, length(lambda[lambda < 1]) / length(lambda))
+# }
+# plot(freqRange, survivalSingle,col="green",ylim=c(0,1),pch=2,xlab="",ylab="")
+# par(new=TRUE)
+
+## model 2 v2
 survivalSingle <- list()
 for (averageMutFreq in freqRange) {
-    lambda <- extendedSim(loci, averageMutFreq, Ne) - Ne
+    lambda <- (genotypeSim(loci, averageMutFreq, N0, birthRate))/N0
     survivalSingle <- append(survivalSingle, length(lambda[lambda < 1]) / length(lambda))
 }
 plot(freqRange, survivalSingle,col="green",ylim=c(0,1),pch=2,xlab="",ylab="")
@@ -200,8 +203,8 @@ par(new=TRUE)
 ## model 3
 survivalRecurse <- list()
 for (averageMutFreq in freqRange) {
-    outcomeGroup <- extendedSimRecurse2(loci, averageMutFreq, Ne, birthRate, carryingCapacity)
-    # fitnessDecreases <- W(loci, mutFreqs, Ne)
+    outcomeGroup <- recursiveSim(loci, averageMutFreq, N0, birthRate, carryingCapacity)
+    # fitnessDecreases <- W(loci, mutFreqs, N0)
     survivalRecurse <- append(survivalRecurse, length(outcomeGroup[outcomeGroup == "EXTINCT"]) / length(outcomeGroup))
 }
 
@@ -224,8 +227,8 @@ slimData <- read.csv("./data/out_AnalyticalComparison_LIFP_1695296487.csv") %>%
   group_by(MutationFrequency, MutationCount, Individuals, GrowthRate) %>% 
   mutate(count = n()) %>%
   mutate(extinctionProbability = sum(Result == "EXTINCT") / count) %>%
-  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate,survivalRate, count) %>% 
+  group_by(MutationFrequency, MutationCount, Individuals, GrowthRate, extinctionProbability, count) %>% 
   summarise()
 
 dataSlice2d.MutationFrequency <- filter(slimData, MutationCount==100, Individuals == 25, GrowthRate == 3)
-plot(survivalRate~MutationFrequency,data=filter(dataSlice2d.MutationFrequency),col="black",ylim=c(0,1),xlim=c(0,0.3),xlab="Deleterious Recessive Frequency",ylab="Extinction Probability")
+plot(extinctionProbability~MutationFrequency,data=filter(dataSlice2d.MutationFrequency),col="black",ylim=c(0,1),xlim=c(0,0.3),xlab="Deleterious Recessive Frequency",ylab="Extinction Probability")
