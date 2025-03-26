51f95ee00819f2d64135fc4a925854f4213866bb
4321louis <45321dumkof@gmail.com>
2025-03-27 00:09:59 +1100

    FitnessAverage test

--- slimulations/LoadedIsolatedFoundingPopulation/setupHPC.sh
+++ slimulations/LoadedIsolatedFoundingPopulation/setupHPC.sh
@@ -1,5 +1,8 @@
 outputFile=$1
-echo "Seed,Result,Time,Males,Individuals,GrowthRate,RecombinationRate,ChromosomeCount,MaxGenerations,CarryingCapacity,MutationFrequency,MutationCount,Sterile,Xlinked,FemaleOnlyEffect" >> "$outputFile"
-
+if [["$outputFile" == *"FitnessAverage"*]]; then
+    echo "Seed,Fitness,GrowthRate,Time,Individuals,Females"
+else
+    echo "Seed,Result,Time,Males,Individuals,GrowthRate,RecombinationRate,ChromosomeCount,MaxGenerations,CarryingCapacity,MutationFrequency,MutationCount,Sterile,Xlinked,FemaleOnlyEffect" >> "$outputFile"
+fi
 
 
